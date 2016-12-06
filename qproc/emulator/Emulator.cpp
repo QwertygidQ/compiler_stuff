132f6090b05a10f944e25448b959d7bb0f9fe133
QwertygidQ <Qwertygid@gmail.com>
2016-12-06 23:31:43 +0300

    A lil change

--- qproc/emulator/Emulator.cpp
+++ qproc/emulator/Emulator.cpp
@@ -11,73 +11,100 @@ Emulator::Emulator(const std::string filename)
     file.open(filename, std::ios::binary | std::ios::ate);
     if (!file.is_open())
         error("Could not open the ROM", false);
-    
+
     std::streampos fsize = file.tellg();
     std::cout << "Filesize: " << fsize << " bytes" << std::endl << std::endl;
     if (fsize > PM_SIZE * sizeof(uint8_t))
         error("Corrupt ROM", false);
-    
+
     file.seekg(0);
-    
+
     size_t PM_Cell = 0;
     while (file.tellg() != fsize)
         file.read(reinterpret_cast<char*>(&PM[PM_Cell++]), sizeof(uint8_t));
-    
+
     file.close();
 }
 void Emulator::run()
 {
+    enum opcodes
+    {
+        NOP    = 0x00,
+        ADD    = 0x01,
+        SUB    = 0x02,
+        NEG    = 0x03,
+        SHL    = 0x04,
+        SHR    = 0x05,
+        AND    = 0x06,
+        OR     = 0x07,
+        XOR    = 0x08,
+        NOT    = 0x09,
+        JMP    = 0x0A,
+        JZ     = 0x0B,
+        JNZ    = 0x0C,
+        PUSH   = 0x0D,
+        RM     = 0x0E,
+        PUSHIP = 0x0F,
+        POPIP  = 0x10,
+        RMIP   = 0x11,
+        PUSHPM = 0x12,
+        POPPM  = 0x13,
+        INPUT  = 0x14,
+        PEEK   = 0x15,
+        HALT   = 0x16
+    };
+
     for (IP = 0; IP < PM_SIZE; IP++)
         switch (PM[IP])
         {
-            case 0x00: //NOP
+            case NOP:
                 break;
-            case 0x01: //ADD
+            case ADD:
                 DS.push(pop_DS() + pop_DS());
                 break;
-            case 0x02: //SUB
+            case SUB:
                 {
                     const int32_t Y = pop_DS(), X = pop_DS();
                     DS.push(X - Y);
                 }
                 break;
-            case 0x03: //NEG
+            case NEG:
                 DS.push(-pop_DS());
                 break;
-            case 0x04: //SHL
+            case SHL:
                 {
                     const int32_t Y = pop_DS(), X = pop_DS();
                     DS.push(X << Y);
                 }
                 break;
-            case 0x05: //SHR
+            case SHR:
                 {
                     const int32_t Y = pop_DS(), X = pop_DS();
                     DS.push(X >> Y);
                 }
                 break;
-            case 0x06: //AND
+            case AND:
                 DS.push(pop_DS() & pop_DS());
                 break;
-            case 0x07: //OR
+            case OR:
                 DS.push(pop_DS() | pop_DS());
                 break;
-            case 0x08: //XOR
+            case XOR:
                 DS.push(pop_DS() ^ pop_DS());
                 break;
-            case 0x09: //NOT
+            case NOT:
                 DS.push(~pop_DS());
                 break;
-            case 0x0A: //JMP
+            case JMP:
                 {
                     const int32_t X = pop_DS();
                     if (is_not_in_bounds(X))
                         error("X is out of bounds in JMP", true);
-                    
+
                     IP = X - 1; //IP will get incremented in this loop afterwards
                 }
                 break;
-            case 0x0B: //JZ
+            case JZ:
                 {
                     const int32_t Y = pop_DS(), X = pop_DS();
                     if (is_not_in_bounds(Y))
@@ -86,7 +113,7 @@ void Emulator::run()
                         IP = Y - 1; //IP will get incremented in this loop afterwards
                 }
                 break;
-            case 0x0C: //JNZ
+            case JNZ:
                 {
                     const int32_t Y = pop_DS(), X = pop_DS();
                     if (is_not_in_bounds(Y))
@@ -95,16 +122,16 @@ void Emulator::run()
                         IP = Y - 1; //IP will get incremented in this loop afterwards
                 }
                 break;
-            case 0x0D: //PUSH
+            case PUSH:
                 if (is_not_in_bounds(IP, PM_SIZE - sizeof(int32_t) - 1))
                     error("PUSH used without a proper operand", true);
                 DS.push(get_data_from_PM(IP + 1));
                 IP += sizeof(int32_t); // IP will get incremented in this loop afterwards
                 break;
-            case 0x0E: //RM
+            case RM:
                 pop_DS();
                 break;
-            case 0x0F: //PUSHIP
+            case PUSHIP:
                 {
                     const int32_t X = pop_DS();
                     if (is_not_in_bounds(X))
@@ -112,16 +139,16 @@ void Emulator::run()
                     IS.push(X);
                 }
                 break;
-            case 0x10: //POPIP
+            case POPIP:
                 IP = pop_IS();
                 if (is_not_in_bounds(IP))
                     error("IP is out of bounds in POPIP", true);
                 IP--; // IP will get incremented in this loop afterwards
                 break;
-            case 0x11: //RMIP
+            case RMIP:
                 pop_IS();
                 break;
-            case 0x12: //PUSHPM
+            case PUSHPM:
                 {
                     const int32_t X = pop_DS();
                     if (is_not_in_bounds(X, PM_SIZE - sizeof(int32_t)))
@@ -129,7 +156,7 @@ void Emulator::run()
                     DS.push(get_data_from_PM(X));
                 }
                 break;
-            case 0x13: //POPPM
+            case POPPM:
                 {
                     int32_t Y = pop_DS();
                     int32_t X = pop_DS();
@@ -139,7 +166,7 @@ void Emulator::run()
                         PM[X++] = reinterpret_cast<int8_t*>(&Y)[sizeof(int32_t) - 1 - i];
                 }
                 break;
-            case 0x14: //INPUT
+            case INPUT:
                 {
                     int32_t input;
                     std::cin >> input;
@@ -148,12 +175,12 @@ void Emulator::run()
                     DS.push(input);
                 }
                 break;
-            case 0x15: //PEEK
+            case PEEK:
                 if (DS.empty())
                     error("Tried to PEEK empty DS", true);
                 std::cout << DS.top() << std::endl;
                 break;
-            case 0x16: //HALT
+            case HALT:
                 return;
             default:
                 error("Unknown instruction", true);
@@ -164,10 +191,10 @@ size_t Emulator::pop_IS()
 {
     if (IS.empty())
         error("Attempted to pop empty IS", true);
-    
+
     size_t top = IS.top();
     IS.pop();
-    
+
     return top;
 }
 
@@ -175,10 +202,10 @@ int32_t Emulator::pop_DS()
 {
     if (DS.empty())
         error("Attempted to pop empty DS", true);
-    
+
     int32_t top = DS.top();
     DS.pop();
-    
+
     return top;
 }
 
@@ -186,15 +213,15 @@ int32_t Emulator::get_data_from_PM(const size_t beginning)
 {
     if (is_not_in_bounds(beginning, PM_SIZE - sizeof(int32_t)))
         error("Variable beginning is out of bounds in get_data_from_PM", true);
-    
+
     size_t bits = sizeof(uint8_t) * CHAR_BIT;
-    
+
     int32_t result = 0;
-    
+
     size_t i = 0;
     for (int j = sizeof(uint32_t) - 1; j >= 0; j--, i++)
         result |= PM[beginning + i] << (bits * j);
-    
+
     return result;
 }
 
@@ -208,6 +235,6 @@ void Emulator::error(const std::string msg, bool print_instr_number)
     std::string exception_text = "ERROR: " + msg;
     if (print_instr_number)
         exception_text += " ; Instruction #" + std::to_string(IP);
-        
+
     throw std::runtime_error(exception_text);
 }
